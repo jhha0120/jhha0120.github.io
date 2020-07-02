22eec7e2fb2cebb13923f0ac4fb4f8211b5c9208
paikwiki <paikwiki@gmail.com>
2020-07-02 15:09:57 +0900

    메뉴 오류 수정

--- assets/custom.js
+++ assets/custom.js
@@ -1,4 +1,7 @@
-// if ( document.querySelector('.artwork-wrapper') !== null ) {
-//   const subNav = document.querySelector('.nav-sub');
-//   subNav.style.display = 'flex';
-// };
+if ( document.querySelector('.artwork-wrapper') !== null ) {
+  const subNav = document.querySelector('.nav-sub');
+  subNav.style.display = 'flex';
+};
+
+// const menuItem = document.querySelectorAll('.nav-main > li');
+// menuItem.forEach((i) => { i.addEventListener('mouseover', (e) => { console.log(e)}) });
