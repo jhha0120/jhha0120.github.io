63a953fb667b235f67677ad8c8caa27d2e7f93b6
paikwiki <paikwiki@gmail.com>
2020-07-02 14:48:56 +0900

    불필요한 자바스크립트 코드 주석처리

--- assets/custom.js
+++ assets/custom.js
@@ -1,4 +1,4 @@
-if ( document.querySelector('.artwork-wrapper') !== null ) {
-  const subNav = document.querySelector('.nav-sub');
-  subNav.style.display = 'flex';
-};
+// if ( document.querySelector('.artwork-wrapper') !== null ) {
+//   const subNav = document.querySelector('.nav-sub');
+//   subNav.style.display = 'flex';
+// };
